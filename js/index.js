4c60eb7f6a11eaf7431a31b4c920f7b5028e4a5c
CeciLam2025 <lambertuccicecilia@gmail.com>
2025-05-26 07:31:19 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,40 +6,38 @@ const navElements = [
   { title: "Comprar", link: "/pags/login.html" }
 ];
 
+// Función global para cerrar sesión
+window.cerrarSesion = function () {
+  localStorage.removeItem("logueado");
+  window.location.href = "/pags/login.html";
+};
 
-const navBar = `
-   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="../images/logo jl.png" alt="Logo" width="100">    
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav">
-                   ${
-                    navElements.map(e =>{
-                        return `
-                        <li class="nav-item">
-                        <a class="nav-link" href=${e.link}>${e.title}</a>
-                        </li>
-                        `
-                    }).join('')
-                   }
-                </ul>
-                
-            </div>
-            
+window.addEventListener('load', () => {
+  const navBar = `
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="../images/logo jl.png" alt="Logo" width="100">    
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav">
+            ${navElements.map(e => `
+              <li class="nav-item">
+                <a class="nav-link" href="${e.link}">${e.title}</a>
+              </li>`).join('')}
+          </ul>
+          <button onclick="cerrarSesion()" class="btn btn-outline-warning ms-auto">Cerrar sesión</button>
         </div>
-           
-       </nav>
-`
+      </div>
+    </nav>
+  `;
 
-let navContainer = document.querySelector('header')
+  let navContainer = document.querySelector('header');
+  navContainer.innerHTML = navBar;
+});
 
-window.addEventListener('load', ()=>{
-    navContainer.innerHTML = navBar
-})
 
 
